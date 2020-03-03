3db16aff58dcbe85f15a3e5bf9c139bfbcf5e236
Simao <lucas.simao@epitech.eu>
2020-03-03 21:27:23 +0100

    [MODIF] generating tree

--- src/tree.hs
+++ src/tree.hs
@@ -7,6 +7,24 @@
 
 module Tree where
 
+getBlinks :: Int -> Int -> String -> String
+getBlinks window count tree
+    | count == window = tree
+    | otherwise = getBlinks window (count + 1) (" " ++ tree)
+
+loop :: Int -> Int -> String -> IO ()
+loop lines count tree
+    | lines == 0 = do
+        putStrLn tree
+        loop lines count (tree ++ "*")
+    | count == lines = return ()
+    | otherwise = do
+        putStrLn tree
+        loop lines (count + 1) (tree ++ "*")
+
 displayTree :: Int -> Int -> Int -> Int -> Int -> IO ()
 displayTree rule lines window start move = do
-    
+    let count = 0
+    let str = "*"
+    let tree = getBlinks window count str
+    loop lines count tree
