594e6c04863706725a30d1c9c457735fdf795eb6
Lucas Simao <lucas.simao@epitech.eu>
2020-03-03 12:31:02 +0100

    [MODIF] main.hs / modif the parsing handling

--- src/main.hs
+++ src/main.hs
@@ -12,13 +12,23 @@ import System.Exit
 import Data.Maybe
 import Data.List
 
+checkExit :: Int -> IO () 
+checkExit rule
+    | rule == -1 = errorExit
+    | otherwise = return ()
+
+errorExit :: IO ()
+errorExit = do
+    putStrLn "Bad inputs flags"
+    exitWith (ExitFailure 84)
+
 defaultVal :: String -> Int
 defaultVal elem
-    | elem == "--rule" = 0
-    | elem == "--lines" = 0
+    | elem == "--rule"   = -1
+    | elem == "--lines"  = 0
     | elem == "--window" = 80
-    | elem == "--start" = 0
-    | elem == "--move" = 0
+    | elem == "--start"  = 0
+    | elem == "--move"   = 0
 
 getPos :: String -> [String] -> Int
 getPos elem list = fromMaybe (defaultVal elem) (elemIndex elem list)
@@ -26,9 +36,16 @@ getPos elem list = fromMaybe (defaultVal elem) (elemIndex elem list)
 main :: IO ()
 main = do
     args <- getArgs
-    let rule = getPos "--rule" args
-    let lines = getPos "--lines" args
+
+    let rule   = getPos "--rule"   args
+    let lines  = getPos "--lines"  args
     let window = getPos "--window" args
-    let start = getPos "--start" args
-    let move = getPos "--move" args
-    putStrLn "FDP"
+    let start  = getPos "--start"  args
+    let move   = getPos "--move"   args
+    checkExit rule
+
+    print rule
+    print lines
+    print window
+    print start
+    print move
